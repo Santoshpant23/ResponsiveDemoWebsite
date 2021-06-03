b32eba99e407efb52babb5853cfcb59598915244
Santosh <pantsantosh23@gmail.com>
2021-06-03 21:50:47 +0545

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+burger = document.querySelector('.burger')
+navbar = document.querySelector('.navbar')
+navlist = document.querySelector('.navlist')
+rightnav = document.querySelector('.rightnav')
+burger.addEventListener('click', () => {
+    rightnav.classList.toggle('v-class-resp')
+    navlist.classList.toggle('v-class-resp')
+    navbar.classList.toggle('h-nav-resp')
+})
+
+// burger = document.querySelector('.burger')
+// navbar = document.querySelector('.navbar')
+// rightnav = document.querySelector('.rightnav')
+
+
+
+// burger.addEventListener('click' , ()=>{
+
+// })
